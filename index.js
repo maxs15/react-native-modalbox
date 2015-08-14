04e11c13130e825ffdd2129339c2d5ae551852cb
Maxime Mezrahi <mmezrahi@blackdivine.com>
2015-08-14 13:56:22 -0400

    open / close methods added, isOpen property no longer available position feature added backdrop feature added code refactored bug fixes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var {
   View,
   StyleSheet,
   PanResponder,
-  Animated
+  Animated,
+  TouchableWithoutFeedback
 } = React;
 
 var Overlay         = require('react-native-overlay');
@@ -14,8 +15,20 @@ var screen          = require('Dimensions').get('window');
 var styles = StyleSheet.create({
 
   wrapper: {
-    flex: 1,
-    backgroundColor: "white"
+    backgroundColor: "white",
+    position: "absolute",
+    height: screen.height,
+    width: screen.width
+  },
+
+  backdrop: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: screen.height,
+    width: screen.width
   }
 
 });
@@ -23,12 +36,16 @@ var styles = StyleSheet.create({
 var Modal = React.createClass({
 
   propTypes: {
-    isOpen: React.PropTypes.bool,
     swipeToClose: React.PropTypes.bool,
     swipeThreshold: React.PropTypes.number,
     onClosed: React.PropTypes.func,
     onOpened: React.PropTypes.func,
-    onClosingState: React.PropTypes.func
+    onClosingState: React.PropTypes.func,
+    position: React.PropTypes.string,
+    backdrop: React.PropTypes.bool,
+    backdropOpacity: React.PropTypes.number,
+    backdropColor: React.PropTypes.string,
+    backdropContent: React.PropTypes.element
   },
 
   getDefaultProps: function () {
@@ -36,30 +53,103 @@ var Modal = React.createClass({
       isOpen: false,
       swipeToClose: true,
       swipeThreshold: 50,
-      aboveStatusBar: true
+      aboveStatusBar: true,
+      position: "center",
+      backdrop: true,
+      backdropOpacity: 0.5,
+      backdropColor: "black",
+      backdropContent: null
     };
   },
 
   getInitialState: function () {
     return {
       position: new Animated.Value(screen.height),
+      backdropOpacity: new Animated.Value(0),
       isAnimateClose: false,
       isAnimateOpen: false,
-      swipeToClose: false
+      swipeToClose: false,
+      height: screen.height,
+      width: screen.width
     };
   },
 
+  componentDidMount: function() {
+    if (this.props.swipeToClose)
+      this.createPanResponder();
+  },
+
+  /****************** ANIMATIONS **********************/
+
+  /*
+   * Open animation for the backdrop, will fade in
+   */
+  animateBackdropOpen: function() {
+    if (this.state.isAnimateBackdrop) {
+      this.state.animBackdrop.stop();
+      this.state.isAnimateBackdrop = false;
+    }
+
+    this.state.isAnimateBackdrop = true;
+    this.state.animBackdrop = Animated.timing(
+      this.state.backdropOpacity,
+      {
+        toValue: 1
+      }
+    );
+    this.state.animBackdrop.start(() => {
+      this.state.isAnimateBackdrop = false;
+    });
+  },
+
+  /*
+   * Close animation for the backdrop, will fade out
+   */
+  animateBackdropClose: function() {
+    if (this.state.isAnimateBackdrop) {
+      this.state.animBackdrop.stop();
+      this.state.isAnimateBackdrop = false;
+    }
+
+    this.state.isAnimateBackdrop = true;
+    this.state.animBackdrop = Animated.timing(
+      this.state.backdropOpacity,
+      {
+        toValue: 0
+      }
+    );
+    this.state.animBackdrop.start(() => {
+      this.state.isAnimateBackdrop = false;
+    });
+  },
+
+  /*
+   * Open animation for the modal, will move up
+   */
   animateOpen: function() {
     if (this.state.isAnimateClose) {
       this.state.animClose.stop();
       this.state.isAnimateClose = false;
     }
 
+    // Backdrop fadeIn
+    if (this.props.backdrop)
+      this.animateBackdropOpen();
+
+    // Detecting modal position
+    this.state.positionDest = 0;
+    if (this.props.position == "bottom") {
+      this.state.positionDest = screen.height - this.state.height;
+    }
+    else if (this.props.position == "center") {
+      this.state.positionDest = screen.height / 2 - this.state.height / 2;
+    }
+
     this.state.isAnimateOpen = true;
     this.state.animOpen = Animated.spring(
       this.state.position,
       {
-        toValue: 0,
+        toValue: this.state.positionDest,
         friction: 8
       }
     );
@@ -69,18 +159,25 @@ var Modal = React.createClass({
     });
   },
 
+  /*
+   * Close animation for the modal, will move down 
+   */
   animateClose: function() {
     if (this.state.isAnimateOpen) {
       this.state.animOpen.stop();
       this.state.isAnimateOpen = false;
     }
 
+    // Backdrop fadeout
+    if (this.props.backdrop)
+      this.animateBackdropClose();
+
     this.state.isAnimateClose = true;
-    this.state.animClose = Animated.spring(
+    this.state.animClose = Animated.timing(
       this.state.position,
       {
         toValue: screen.height,
-        friction: 10
+        duration: 400
       }
     );
     this.state.animClose.start(() => {
@@ -90,6 +187,10 @@ var Modal = React.createClass({
     });
   },
 
+  /*
+   * Create the pan responder to detect gesture
+   * Only used if swipeToClose is enabled
+   */
   createPanResponder: function() {
     var closingState = false;
 
@@ -100,15 +201,15 @@ var Modal = React.createClass({
         this.animateOpen();
     };
 
-    var animEvt = Animated.event([null, {dy: this.state.position}]);
+    var animEvt = Animated.event([null, {customY: this.state.position}]);
 
     var onPanMove = (evt, state) => {
       var newClosingState = (state.dy > this.props.swipeThreshold) ? true : false;
-
       if (state.dy < 0) return;
       if (newClosingState != closingState && this.props.onClosingState)
         this.props.onClosingState(newClosingState);
       closingState = newClosingState;
+      state.customY = state.dy + this.state.positionDest;
 
       animEvt(evt, state);
     };
@@ -122,34 +223,75 @@ var Modal = React.createClass({
     });
   },
 
-  componentWillReceiveProps: function(props) {
-    if (props.isOpen && !this.state.isAnimateOpen) {
-      this.animateOpen();
-    }
-    else if (!props.isOpen && !this.state.isAnimateClose)
-      this.animateClose();
-
-    if (props.swipeToClose != this.state.swipeToClose) {
-      this.state.swipeToClose = props.swipeToClose;
-      if (props.swipeToClose)
-        this.createPanResponder();
-      else
-        this.state.pan = null;
-    }
+  /*
+   * Event called when the modal view layout is calculated
+   */
+  onViewLayout: function(evt) {
+    this.state.height = evt.nativeEvent.layout.height;
+    this.state.width = evt.nativeEvent.layout.width;
+    if (this.onViewLayoutCalculated) this.onViewLayoutCalculated();
   },
 
+  /*
+   * Render the backdrop element 
+   */
+  renderBackdrop: function() {
+    var backdrop  = [];
+
+    if (this.props.backdrop) {
+      backdrop = (
+        <TouchableWithoutFeedback onPress={this.animateClose}>
+          <Animated.View style={[styles.backdrop, {opacity: this.state.backdropOpacity}]}>
+            <View style={[styles.backdrop, {backgroundColor:this.props.backdropColor, opacity: this.props.backdropOpacity}]}/>
+            {this.props.backdropContent || []}
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      );
+    }
+
+    return backdrop;
+  },
+
+  /*
+   * Render the component
+   */
   render: function() {
-    var visible  = this.props.isOpen || this.state.isAnimateOpen || this.state.isAnimateClose;
-    var pan = this.state.pan ? this.state.pan.panHandlers : {};
+    var visible   = this.state.isAnimateOpen || this.state.isAnimateClose;
+    var pan       = this.state.pan ? this.state.pan.panHandlers : {};
+    var offsetX   = (screen.width - this.state.width) / 2;
+    var backdrop  = this.renderBackdrop();
 
     return (
       <Overlay isVisible={visible} aboveStatusBar={this.props.aboveStatusBar}>
-        <Animated.View style={[styles.wrapper, this.props.style, {transform: [{translateY: this.state.position}]} ]} {...pan}>
+        {backdrop}
+        <Animated.View
+         onLayout={this.onViewLayout}
+         style={[styles.wrapper, this.props.style, {transform: [{translateY: this.state.position}, {translateX: offsetX}]} ]}
+         {...pan}>
           {this.props.children}
         </Animated.View>
       </Overlay>
     );
+  },
+
+  /****************** PUBLIC METHODS **********************/
+
+  open: function() {
+    if (!this.state.isAnimateOpen) {
+      this.onViewLayoutCalculated = () => {
+        this.setState({});
+        this.animateOpen();
+      };
+      this.setState({isAnimateOpen : true});
+    }
+  },
+
+  close: function() {
+    if (!this.state.isAnimateClose) {
+      this.animateClose();
+    }
   }
+
 
 });
 
