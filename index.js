4739a266e46f81db5263b10cccf8713ef8e5e0af
moay <mv@moay.de>
2019-11-27 13:08:14 +0100

    Fix reopening modal
    In our setup, this part of the code triggered reopening of the modal immediately after closing, because the modal is still being animated when the `onPanRelease` kicks in.

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ export default class ModalBox extends React.PureComponent {
           : state.dy > this.props.swipeThreshold
       ) {
         this.close();
-      } else if (!this.state.isOpen) {
+      } else if (!this.state.isAnimateClose && !this.state.isOpen) {
         this.animateOpen();
       }
     };
